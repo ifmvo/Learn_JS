ded7f56f2b3303701142ef2fdc357409f2b4c702
Matthew_Chen <Matthew_Chen_1994@163.com>
2017-05-18 18:41:38 +0800

    learn function

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 /**
  * Created by ifmvo on 17-5-18.
  */
+
+var pList = document.getElementsByTagName("p");
+alert(pList.length);
+
+for (var i=0; i<pList.length; i++){
+    var title = pList[i].getAttribute("title");
+    if (title != null){
+        alert(title);
+    }
+}
